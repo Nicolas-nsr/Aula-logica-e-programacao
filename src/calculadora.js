cfa5b33a7cb051bfc41868938820163963fd35d3
Nicolas.nsr <nickolascp@hotmail.com>
2025-08-28 23:45:52 -0300

    atualização de dados de calculadora e dados para ela

---
+++ src/calculadora.js
@@ -0,0 +1,25 @@
+function somarNumeros (valor1 ,valor2){
+    const resultado = valor1 + valor2;
+    return resultado;
+}
+
+function calcularMediaDeDoisNumeros (valor1, valor2){
+    //1. Soma todos os valores
+    const resultadoDaSomaDeDoisValores = somarNumeros (valor1, valor2);
+
+    //2. Divide pela quantidade de números passados
+    const resultadoMediaDeDoisNumeros = resultadoDaSomaDeDoisValores/2;
+
+    //3. retornar resultado
+    return resultadoMediaDeDoisNumeros;
+}
+
+function realizarSomaDosCamposDaTela(){
+    const valor1 = [];//primeiro valor
+    const valor2 = [];//segundo valor
+    const resultado = SomarNumeros(valor1, valor2);
+    print = CaixaResultado(resultado);
+}
+module.exports = {
+    somarNumeros
+}
---
+++ tests/calculadora.test.js
@@ -0,0 +1,5 @@
+const {somarNumeros} = require('../src/calculadora');
+
+const resultadoDaSoma = somarNumeros(5 , 3);
+
+console.log (resultadoDaSoma);
